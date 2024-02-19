f0d52935825685c6ced3061c1a08ddae2ead4b7f
avinashmohare <mohareavinash80@gmail.com>
2024-02-20 01:14:55 +0530

    Added Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,9 +16,10 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" className="dark">
             <body className={inter.className}>
-                <h2>Nav Items</h2>
+                <div className="relative w-full flex items-center justify-center"></div>
+                <Navbar />
                 {children}
             </body>
         </html>
