3cd0c242cb4c2b266ebd5c8efe3a79bc387f853f
avinashmohare <mohareavinash80@gmail.com>
2024-02-24 00:28:25 +0530

    Added Sticky Scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedCourses from "@/components/featuredCourses";
 import HeroSection from "@/components/heroSection";
+import WhyChooseUs from "@/components/whyChooseUs";
 
 export default function Home() {
     return (
         <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
             <HeroSection />
             <FeaturedCourses />
+            <WhyChooseUs />
         </main>
     );
 }
