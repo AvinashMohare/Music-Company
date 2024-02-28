39a5540079253627e04a3a2594d4647628dca737
avinashmohare <mohareavinash80@gmail.com>
2024-02-29 00:39:00 +0530

    Instructors section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import FeaturedCourses from "@/components/featuredCourses";
 import HeroSection from "@/components/heroSection";
+import Instructors from "@/components/instructors";
+import TestimonialCards from "@/components/testimonialCards";
+import UpcomingWebinars from "@/components/upcomingWebinars";
 import WhyChooseUs from "@/components/whyChooseUs";
 
 export default function Home() {
@@ -8,6 +11,9 @@ export default function Home() {
             <HeroSection />
             <FeaturedCourses />
             <WhyChooseUs />
+            <TestimonialCards />
+            <UpcomingWebinars />
+            <Instructors />
         </main>
     );
 }
